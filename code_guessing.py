0997ac460d7ce4a08bff01c9fe0dff2563a7f4ce
Ethan Cloin <psnethan@gmail.com>
2022-05-30 19:52:06 -0400

    troubleshooting guesslang

---
+++ code_guessing.py
@@ -0,0 +1,20 @@
+from guesslang import Guess
+import streamlit as st
+
+def guess_test(code_string: str):
+    guess = Guess()
+    return guess.language_name(code_string)
+
+
+if __name__ == "__main__":
+    # client.run(TOKEN)
+    test_result = guess_test(
+        """
+    print("Hello World!") 
+    """
+    )
+    print(test_result)
+
+code = st.text_input()
+if st.button("TRY"):
+    guess_test(code)
--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 import discord
 import os
 from dotenv import load_dotenv
+from guesslang import Guess
 
 
-
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-print(TOKEN)
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-
-def parseCode(message):
-    
-    print("")
+# load_dotenv()
+# TOKEN = os.getenv("DISCORD_TOKEN")
+# print(TOKEN)
+# client = discord.Client()
 
 
+# @client.event
+# async def on_ready():
+#     print(f"{client.user} has connected to Discord!")
 
-@client.event
-async def on_message(message):
-    parseCode(message)
 
-client.run(TOKEN)
+# def parseCode(message):
+#
+#     print("")
+
+
+# @client.event
+# async def on_message(message):
+#     parseCode(message)
+
+
+def guess_test(code_string: str):
+    guess = Guess()
+    return guess.language_name(code_string)
+
+
+if __name__ == '__main__':
+    # client.run(TOKEN)
+    test_result = guess_test("""
+    print("Hello World!") 
+    """)
+    print(test_result)
