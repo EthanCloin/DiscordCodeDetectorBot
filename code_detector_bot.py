282f99c67fda04ba805267f98c0c1e7bef27a107
Andreas Ink <andreasink17@gmail.com>
2022-05-30 20:29:23 -0400

    Discord setup

--- code_detector_bot.py
+++ code_detector_bot.py
@@ -4,10 +4,28 @@ from dotenv import load_dotenv
 from guesslang import Guess
 
 
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+print(TOKEN)
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f"{client.user} has connected to Discord!")
+
 # react to event for when user sends message
 
+@client.event
+async def on_message(message):
+    guess = Guess()
+    if guess.language_name(code_string) != "Batchfile" {
+        # send popup message to user suggesting a block comment
+        await message.channel.send("Stop it get some help, format your code lol")
+    }
+    
+    
 
-# send popup message to user suggesting a block comment
+
 
 
 
--- code_guessing.py
+++ code_guessing.py
@@ -1,6 +1,39 @@
 from guesslang import Guess
 import streamlit as st
 
+import discord
+import os
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+print(TOKEN)
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f"{client.user} has connected to Discord!")
+    st.success("Discord is up!")
+
+# react to event for when user sends message
+
+@client.event
+async def on_message(message):
+    guess = Guess()
+    if guess.language_name(code_string) != "Batchfile" {
+        # send popup message to user suggesting a block comment
+        await message.channel.send("Stop it get some help, format your code lol")
+    }
+    
+    
+
+
+
+
+
+# automatically delete and reformat user message
 
 def guess_test(code_string: str):
     guess = Guess()
