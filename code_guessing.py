43948f44dfcb5e1996956366cc35a32e28a04672
Andreas Ink <andreasink17@gmail.com>
2022-05-30 20:50:24 -0400

    Working in replit

--- code_guessing.py
+++ code_guessing.py
@@ -1,31 +1,32 @@
 from guesslang import Guess
-import streamlit as st
+
 import discord
 import os
 from dotenv import load_dotenv
 
 
-
 load_dotenv()
-TOKEN = st.secrets["DISCORD_TOKEN"]
+
+# TOKEN = os.getenv("DISCORD_TOKEN")
+# TOKEN = os.environ['DISCORD_TOKEN']
 print(TOKEN)
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print(f"{client.user} has connected to Discord!")
-    st.success("Discord is up!")
+    
 
 # react to event for when user sends message
 
 @client.event
 async def on_message(message):
     guess = Guess()
-    if guess.language_name(code_string) != "Batchfile":
+    if guess.language_name(message.text) != "Batchfile":
         # send popup message to user suggesting a block comment
         await message.channel.send("Stop it get some help, format your code lol")
     
-    
+
     
 
 
@@ -34,18 +35,13 @@ async def on_message(message):
 
 # automatically delete and reformat user message
 
-def guess_test(code_string: str):
-    guess = Guess()
-    return guess.language_name(code_string)
 
 
 if __name__ == "__main__":
     # client.run(TOKEN)
     pass
 
-code = st.text_area("Code")
-if st.button("TRY"):
-    language = guess_test(code)
-    st.success(language)
+
+
 
 client.run(TOKEN)
