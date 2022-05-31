e787cb0ffe51371ee52e5ab993c41d292e7ae4f2
Ethan Cloin <psnethan@gmail.com>
2022-05-30 22:45:43 -0400

    trying subclass version

--- code_detector_bot.py
+++ code_detector_bot.py
@@ -1,51 +1,78 @@
 import discord
-from discord.ext import commands
-import os
-from dotenv import load_dotenv
 from guesslang import Guess
-
-load_dotenv()
-
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-bot = commands.Bot(command_prefix="!")
-
-load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
-print(TOKEN)
-client = discord.Client()
+from config import Config
 
 
-@client.event
-async def on_ready():
-    print(f"{client.user} has connected to Discord!")
+class CodeDetector(discord.Client):
+    """extension of dicord Client object with added code detector behavior"""
+
+    def __init__(self, discord_token: str, config: Config, **options):
+        super().__init__(**options)
+        self.discord_token = discord_token
+        self.config = config
+
+    # listeners
+    async def on_ready(self):
+        print(f"{self.user} has connected to Discord!")
+
+    async def on_message(self, message):
+        guess = Guess()
+        if guess.language_name(message.content) != "Batchfile":
+            # send popup message to user suggesting a block comment
+            await message.channel.send("Stop it get some help, format your code lol")
+
+    async def on_reaction_add(self, reaction, user):
+        message = reaction.message
+        emoji = reaction.emoji
+        if user.bot:
+            return
+
+        if emoji == "👨‍💻":
+            guess = Guess()
+            programming_lang = guess.language_name(message.content)
+
+            if programming_lang != "Batchfile" and programming_lang != "INI":
+                # send popup message to user suggesting a block comment
+                await message.channel.send(
+                    "Stop it get some help, format your code lol"
+                )
+            return
+
+        else:
+            return
 
 
-# react to event for when user sends message
-
-
-@client.event
-async def on_message(message):
-    guess = Guess()
-    code = guess.language_name(message.content)
-    print(code)
-    if code != "Batchfile" and code != "INI":
-        # send popup message to user suggesting a block comment
-        await message.channel.send("Stop it get some help, format your code lol")
-
-
-# send popup message to user suggesting a block comment
-@bot.command(name="my_cmd")
-async def command_testing(ctx):
-    response = "you have commanded, i have complied"
-    await ctx.send(response)
-
-
-def suggest_language_formatted():
-    pass
-
-
-# automatically delete and reformat user message
+# @client.event
+# async def on_ready():
+#     print(f"{client.user} has connected to Discord!")
+#
+#
+# # react to event for when user sends message
+#
+#
+# @client.event
+# async def on_message(message):
+#     guess = Guess()
+#     code = guess.language_name(message.content)
+#     print(code)
+#     if code != "Batchfile" and code != "INI":
+#         # send popup message to user suggesting a block comment
+#         await message.channel.send("Stop it get some help, format your code lol")
+#
+#
+# # send popup message to user suggesting a block comment
+# @bot.command(name="my_cmd")
+# async def command_testing(ctx):
+#     response = "you have commanded, i have complied"
+#     await ctx.send(response)
+#
+#
+# def suggest_language_formatted():
+#     pass
+#
+#
+# # automatically delete and reformat user message
 
 
 if __name__ == "__main__":
-    bot.run(DISCORD_TOKEN)
+    pass
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import discord
 import os
 from dotenv import load_dotenv
-from guesslang import Guess
-import subprocess
 import logging
+from code_detector_bot import CodeDetector
+from config import Config
 
-logging.basicConfig(filename="logs/bot_actions.log", filemode="a", level="DEBUG")
-_log = logging.getLogger(__name__)
+# logging.basicConfig(filename="logs/bot_actions.log", filemode="a", level="DEBUG")
+# _log = logging.getLogger(__name__)
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+CONFIG = Config()
+
 
 if __name__ == "__main__":
-    subprocess.run("python3 code_guessing.py")
+    client = CodeDetector(DISCORD_TOKEN, CONFIG)
+    client.run()
