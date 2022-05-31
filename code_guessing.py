2ddc5d1d3a2029b2397c51535041785427f7e86f
Andreas Ink <andreasink17@gmail.com>
2022-05-30 21:43:56 -0400

    Reaction code

--- code_guessing.py
+++ code_guessing.py
@@ -17,33 +17,45 @@ async def on_ready():
 
 
 # react to event for when user sends message
-@client.event
-async def on_message(message):
-    guess = Guess()
-    if guess.language_name(message.content) != "Batchfile":
-        # send popup message to user suggesting a block comment
-        await message.channel.send("Stop it get some help, format your code lol")
+# @client.event
+# async def on_message(message):
+#     guess = Guess()
+#     if guess.language_name(message.content) != "Batchfile":
+#         # send popup message to user suggesting a block comment
+#         await message.channel.send("Stop it get some help, format your code lol")
 
 @client.event
 async def on_reaction_add(reaction, user):
-    embed = reaction.embeds[0]
+    embed = reaction.message
     emoji = reaction.emoji
-
+    print(embed)
+    print(emoji)
     if user.bot:
         return
 
-    if emoji == "emoji 1":
-        # fixed_channel = client.get_channel(channel_id)
-        # await fixed_channel.send(embed=embed)
-    elif emoji == "emoji 2":
-        #do stuff
-    elif emoji == "emoji 3":
-        #do stuff
+    if emoji == "👨‍💻":
+      guess = Guess()
+      code = guess.language_name(embed.content)
+      print(code)
+      if  code != "Batchfile" and code != "INI":
+          # send popup message to user suggesting a block comment
+          await embed.channel.send("Stop it get some help, format your code lol")
+      return
+    
     else:
         return
+
+
+
 # automatically delete and reformat user message
+
+
+
 if __name__ == "__main__":
+    # client.run(TOKEN)
     pass
+
+
 
 
 client.run(TOKEN)
