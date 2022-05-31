87edd3c8917075568d0406036dd69a28abf17dbb
Ethan Cloin <psnethan@gmail.com>
2022-05-30 20:11:28 -0400

    troubleshooting streamlit

---
+++ code_detector_bot.py
@@ -0,0 +1,14 @@
+import discord
+import os
+from dotenv import load_dotenv
+from guesslang import Guess
+
+
+# react to event for when user sends message
+
+
+# send popup message to user suggesting a block comment
+
+
+
+# automatically delete and reformat user message
