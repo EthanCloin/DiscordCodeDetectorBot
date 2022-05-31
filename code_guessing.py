ea86c308cc118ca1d8d3cfb4357490515d035b2f
Ethan Cloin <psnethan@gmail.com>
2022-05-30 20:59:50 -0400

    format for linode vm

--- code_guessing.py
+++ code_guessing.py
@@ -7,41 +7,27 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# TOKEN = os.getenv("DISCORD_TOKEN")
-# TOKEN = os.environ['DISCORD_TOKEN']
-print(TOKEN)
+TOKEN = os.getenv("DISCORD_TOKEN")
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
     print(f"{client.user} has connected to Discord!")
-    
+
 
 # react to event for when user sends message
-
 @client.event
 async def on_message(message):
     guess = Guess()
-    if guess.language_name(message.text) != "Batchfile":
+    if guess.language_name(message.content) != "Batchfile":
         # send popup message to user suggesting a block comment
         await message.channel.send("Stop it get some help, format your code lol")
-    
-
-    
-
-
-
 
 
 # automatically delete and reformat user message
-
-
-
 if __name__ == "__main__":
-    # client.run(TOKEN)
     pass
-
-
 
 
 client.run(TOKEN)
